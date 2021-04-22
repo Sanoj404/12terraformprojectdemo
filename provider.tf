e4db799ed683708ab441c7f1ad9a11e896f7bfeb
Sanoj404 <56234552+Sanoj404@users.noreply.github.com>
2021-04-22 22:34:05 +0530

    S3 backend updated

--- provider.tf
+++ provider.tf
@@ -1,3 +1,9 @@
 provider "aws" {
   region = var.region
 }
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-up-and-running-state"
+    key            = "Terraform"
+    region         = "us-east-1"
